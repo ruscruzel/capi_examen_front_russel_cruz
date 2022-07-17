7d6d9a7ede5701a49d9cf47951f25721804291d4
ruscruzel <ruscruzel@gmail.com>
2022-07-16 23:41:00 -0500

    componet tabla usuarios

--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { TablaUsuariosComponent } from './tabla-usuarios/tabla-usuarios.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TablaUsuariosComponent],
+  imports: [CommonModule],
+  exports: [TablaUsuariosComponent],
 })
-export class UsuariosModule { }
+export class UsuariosModule {}
