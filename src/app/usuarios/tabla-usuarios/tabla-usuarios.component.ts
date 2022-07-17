0ac68fe895f8dadd04332419e5edc18aa21e65be
ruscruzel <ruscruzel@gmail.com>
2022-07-17 02:34:21 -0500

    service
    consumo de api para usuarios

--- src/app/usuarios/tabla-usuarios/tabla-usuarios.component.ts
+++ src/app/usuarios/tabla-usuarios/tabla-usuarios.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/rest.service';
 
 @Component({
   selector: 'app-tabla-usuarios',
   templateUrl: './tabla-usuarios.component.html',
-  styleUrls: ['./tabla-usuarios.component.css']
+  styleUrls: ['./tabla-usuarios.component.css'],
 })
 export class TablaUsuariosComponent implements OnInit {
+  public listaUsuarios: any = [];
 
-  constructor() { }
+  constructor(private RestService: RestService) {}
 
   ngOnInit(): void {
+    this.cargarData();
   }
 
+  public cargarData() {
+    this.RestService.get(
+      'http://capiexamenbackrusselcruz.test/users'
+    ).subscribe((respuesta) => {
+      console.log('respuesta:', respuesta);
+      this.listaUsuarios = respuesta;
+    });
+  }
 }
